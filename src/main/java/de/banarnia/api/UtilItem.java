0a49f79009b7c211bc71ba49a7105f22086288b2
Mike Nipkow <mail@mike-nipkow.com>
2024-09-20 13:23:45 +0200

    Initial commit

---
+++ src/main/java/de/banarnia/api/UtilItem.java
@@ -0,0 +1,19 @@
+package de.banarnia.api;
+
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+public class UtilItem {
+
+    public static ItemStack getPlayerSkull(OfflinePlayer offlinePlayer) {
+        ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skull = (SkullMeta) item.getItemMeta();
+        skull.setDisplayName(offlinePlayer.getName());
+        skull.setOwningPlayer(offlinePlayer);
+        item.setItemMeta(skull);
+        return item;
+    }
+
+}
